d5f9f60706a1f75b6579e5b2f83e2b939a9ad054
Marharyta Amosava <mozheliukrita@gmail.com>
2024-12-22 23:02:29 +0100

    TMS Lesson 23
    Added a json files parser to convert files to list, then this list is converted to xml file with showing a max pages book in Console - updated xml structure

--- teachmeskills.hw.lesson_23/src/main/java/com/teachmeskills/hw/lesson_23/model/Library.java
+++ teachmeskills.hw.lesson_23/src/main/java/com/teachmeskills/hw/lesson_23/model/Library.java
@@ -1,13 +1,15 @@
 package com.teachmeskills.hw.lesson_23.model;
 
-import jakarta.xml.bind.annotation.XmlElement;
-import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.*;
 
 import java.util.List;
 
 @XmlRootElement(name = "library")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Library {
 
+    @XmlElementWrapper
+    @XmlElement(name = "book")
     List<Book> books;
 
     public Library() {}
@@ -16,7 +18,6 @@ public class Library {
         this.books = books;
     }
 
-    @XmlElement(name = "book")
     public List<Book> getBooks() {
         return books;
     }
